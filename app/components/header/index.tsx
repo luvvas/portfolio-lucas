420dbc90a44853b696f9b179758b5ef52d6ecbb3
luvvas <lucasmachado1@ufpr.br>
2023-05-13 17:12:17 -0300

    adding isActive link

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,8 +18,8 @@ const NAV_ITENS = [
 
 export const Header = () => {
   return (
-    <header>
-      <div className="container">
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+      <div className="container flex items-center justify-between">
         <Link href="/">
           <Image 
             width={50}
@@ -26,7 +28,7 @@ export const Header = () => {
             alt="Logo"
           />
         </Link>
-        <nav className="flex items-center gap-4">
+        <nav className="flex items-center gap-4 sm:gap-10">
           {NAV_ITENS.map(item => (
             // {...item} is equal to label={label} href={href}
             <NavItem {...item} key={item.label} />
@@ -34,6 +36,5 @@ export const Header = () => {
         </nav>
       </div>
     </header>
-
   )
 }
--- app/components/header/nav-item.tsx
+++ app/components/header/nav-item.tsx
@@ -1,4 +1,6 @@
+import { cn } from "@/app/lib/utils";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 type NavItemProps = {
   label: string;
@@ -6,8 +8,16 @@ type NavItemProps = {
 }
 
 export const NavItem = ({ label, href }: NavItemProps) => {
+  // this hook get the actual pathname
+  const pathname = usePathname();
+
+  // if the current route is equal to the route that the user clicked
+  const isActive = pathname === href;
+
   return (
-    <Link href={href} className="text-gray-400 flex items-center gap-2 font-medium font-mono">
+    <Link href={href} className={cn(
+      "text-gray-400 flex items-center gap-2 font-medium font-mono", isActive && 'text-gray-50'
+    )}>
       <span className="text-emerald-400">#</span>
       { label }
     </Link>
