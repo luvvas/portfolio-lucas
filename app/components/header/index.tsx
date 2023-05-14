a54e448140d502070ab359623eadc0aa0cd5dd8f
luvvas <lucasmachado1@ufpr.br>
2023-05-13 23:22:53 -0300

    adding contact buttons

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -21,12 +21,7 @@ export const Header = () => {
     <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <Image 
-            width={50}
-            height={49}
-            src="/images/logo.svg"
-            alt="Logo"
-          />
+          <h1 className="text-2xl">Lukita</h1>
         </Link>
         <nav className="flex items-center gap-4 sm:gap-10">
           {NAV_ITENS.map(item => (
