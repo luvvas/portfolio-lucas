3e5383e70cda0540b5cc4b2e2730cda6ac8ea8a9
luvvas <lucasmachado1@ufpr.br>
2023-05-30 15:10:00 -0300

    feat: add dynamic work experiences data

--- app/components/pages/home/work-experience/experience-item.tsx
+++ app/components/pages/home/work-experience/experience-item.tsx
@@ -1,18 +1,49 @@
 import Image from 'next/image'
+import { differenceInMonths, differenceInYears, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 import { TechBadge } from '@/app/components/tech-badge'
+import { RichText } from '@/app/components/rich-text'
 
-export const ExperienceItem = () => {
+import { WorkExperience as IWorkExperience } from '@/app/types/work-experience'
+
+type ExperienceItemProps = {
+  experience: IWorkExperience
+}
+
+export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
+  // Da de fazer assim
+  // const { companyLogo, companyName, companyUrl } = experience
+
+  const startDate = new Date(experience.startDate)
+  const formattedStardDate = format(startDate, 'MMM yyyy', { locale: ptBR })
+  const formattedEndDate = experience.endDate ? format(new Date(experience.endDate), 'MMM yyyy', { locale: ptBR }) : 'o momento'
+
+  const end = experience.endDate ? new Date(experience.endDate) : new Date()
+  const months = differenceInMonths(end, startDate) 
+  const years = differenceInYears(end, startDate)
+  const monthsRemaining = months % 12
+
+  const formattedDuration =
+    years > 0
+      ? `${years} ano${years > 1 ? 's' : ''}${
+          monthsRemaining > 0
+            ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? 'es' : ''}`
+            : ''
+        }`
+      : `${months} mes${months > 1 ? 'es' : ''}`
+
+
   return (
     <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
       <div className="flex flex-col items-center">
         <div className="rounded-full border border-gray-500 p-0.5">
           <Image
-            src="/images/rounded.png"
+            src={experience.companyLogo.url}
             width={40}
             height={40}
-            className="opacity-20 rounded-full"
-            alt="Logo da empresa"
+            className="opacity-60 rounded-full"
+            alt={`Logo da empresa ${experience.companyName}`}
           />
         </div>
         <div className="h-full w-[1px] bg-gray-800"></div>
@@ -21,31 +52,27 @@ export const ExperienceItem = () => {
       <div>
         <div className="flex flex-col gap-2 text-sm sm:text-base">
           <a
-            href="https://www.linkedin.com"
+            href={experience.companyUrl}
             target="_blank"
             className="text-gray-500 hover:text-emerald-500 transition-colors"
           >
-            @ Nome da Empresa
+            {experience.companyName}
           </a>
-          <h4 className="text-gray-400">Desenvolvedor C# ASP.NET</h4>
+          <h4 className="text-gray-400">{experience.role}</h4>
           <span className="text-gray-500">
-            out 2022 • No momento • (6 meses)
+            {formattedStardDate} • {formattedEndDate} • ({formattedDuration})
           </span>
-          <p className="text-gray-400">
-            Desenvolvimento e manutenção de serviços utilizando C#, ASP.NET,
-            PostgreSQL e Entity Framework Core, utilizando o Jira para
-            planejamento dos sprints
-          </p>
+          <div className="text-gray-400">
+            <RichText content={experience.description.raw}/>
+          </div>
 
           <p className="text-gray-400 text-sm mb-3 mt-6 font-semibold">
             Competências
           </p>
           <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
-            <TechBadge name="C#" />
-            <TechBadge name="C#" />
-            <TechBadge name="C#" />
-            <TechBadge name="C#" />
-            <TechBadge name="C#" />
+            {experience.technologies.map((tech) => (
+              <TechBadge key={`experience-${experience.companyName}-tech-${tech.name}`} name={tech.name} />
+            ))}
           </div>
         </div>
       </div>
--- app/components/pages/home/work-experience/index.tsx
+++ app/components/pages/home/work-experience/index.tsx
@@ -1,7 +1,13 @@
 import { SectionTitle } from '@/app/components/section-title'
 import { ExperienceItem } from './experience-item'
 
-export const WorkExperience = () => {
+import { WorkExperience as IWorkExperience } from '@/app/types/work-experience'
+
+type WorkExperienceProps = {
+  experiences: IWorkExperience[]
+}
+
+export const WorkExperience = ({ experiences }: WorkExperienceProps) => {
   return (
     <section className="container py-16 flex gap-10 md:gap-4 lg:gap-16 flex-col md:flex-row">
       <div className="max-w-[420px]">
@@ -15,8 +21,12 @@ export const WorkExperience = () => {
       </div>
 
       <div className="flex flex-col gap-4">
-        <ExperienceItem />
-        <ExperienceItem />
+        {experiences?.map(experience => (
+          <ExperienceItem 
+            key={experience.companyName} 
+            experience={experience}
+          />
+        ))}
       </div>
     </section>
   )
