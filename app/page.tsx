705571922dd70cc8d4b02c3017b71eeb62a3443e
luvvas <lucasmachado1@ufpr.br>
2023-05-30 12:19:20 -0300

    feat: add graphql query

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,46 @@ import { KnownTechs } from './components/pages/home/known-techs'
 import { HighlightedProjects } from './components/pages/home/highlighted-projects'
 import { WorkExperience } from './components/pages/home/work-experience'
 
+import { fetchHygraphQuery } from './utils/fetch-hygraph-query'
+
+import { HomePageData } from './types/page-info'
+
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+
+  return fetchHygraphQuery(query, 60 * 60 * 24)
+}
+
 export default async function Home() {
+  const { page: pageData } = await getPageData()
+
   return (
     <>
-      <HeroSection />
-      <KnownTechs />
+      <HeroSection homeInfo={pageData} />
+      <KnownTechs techs={pageData.knownTechs}/>
       <HighlightedProjects />
       <WorkExperience />
     </>
