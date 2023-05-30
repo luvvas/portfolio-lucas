b60d2dd445b7d09af0f0b50e1202066f13dd0362
luvvas <lucasmachado1@ufpr.br>
2023-05-30 15:08:41 -0300

    feat: add work experiences data types

---
+++ app/types/work-experience.ts
@@ -0,0 +1,18 @@
+import type { RichTextContent } from '@graphcms/rich-text-types'
+
+import { KnownTech } from "./projects"
+
+export type WorkExperience = {
+  companyLogo: {
+    url: string
+  }
+  role: string
+  companyName: string
+  companyUrl: string
+  startDate: string
+  endDate: string
+  technologies: KnownTech[]
+  description: {
+    raw: RichTextContent
+  }
+}
