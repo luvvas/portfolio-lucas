da96888836deb8dbdd4791c9c3b04e39431750ec
luvvas <lucasmachado1@ufpr.br>
2023-06-01 16:46:16 -0300

    feat: add dynamic projects data

--- app/projects/projects-list/project-card/index.tsx
+++ app/projects/projects-list/project-card/index.tsx
@@ -1,14 +1,22 @@
 import Image from 'next/image'
 
-export const ProjectCard = () => {
+import { Project } from '@/app/types/projects'
+
+type ProjectCardProps = {
+  project: Project
+}
+
+export const ProjectCard = ({ project }:ProjectCardProps ) => {
+  const technologies = project.technologies.map(x => x.name).join(', ')
+
   return (
     <div className="rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-emerald-400 opacity-70 hover:opacity-100 transition-all group">
       <div className="w-full h-48 overflow-hidden">
         <Image
-          src="/images/card-bg.png"
+          src={project.thumbnail.url}
           width={380}
           height={200}
-          alt="Thumbnail do projeto"
+          alt={`Thumbnail do projeto ${project.title}`}
           unoptimized
           className="w-full h-full object-cover group-hover:scale-110 duration-500 transition-all"
         />
@@ -16,16 +24,13 @@ export const ProjectCard = () => {
 
       <div className="flex-1 flex flex-col p-8">
         <strong className="font-medium text-gray-50/90 group-hover:text-emerald-500 transition-all">
-          Nome do Projeto
+        {project.title}
         </strong>
         <p className="mt-2 text-gray-400 line-clamp-4">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus,
-          fugit cum non sit consequuntur corrupti tempore, beatae expedita animi
-          modi magni voluptatum consectetur. Enim consectetur fugit sed error
-          nulla mollitia earum ratione est optio minus.
+          {project.shortDescription}
         </p>
         <span className="text-gray-300 text-sm font-medium block mt-auto truncate">
-          Next.js, ReactJS, Typescript
+          {technologies}
         </span>
       </div>
     </div>
