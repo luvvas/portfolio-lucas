205761c4b95679f0db4d839cf4e09a4d7952a8dc
luvvas <lucasmachado1@ufpr.br>
2023-06-01 17:04:59 -0300

    feat: add project details query

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,9 +1,55 @@
 import { ProjectDetails } from '@/app/components/pages/project/project-details'
 
-export default function Project() {
+import { fetchHygraphQuery } from '@/app/utils/fetch-hygraph-query'
+
+import { ProjectPageData } from '@/app/types/page-info'
+
+type ProjectProps = {
+  params: {
+    slug: string
+  }
+}
+
+const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
+  const query = `
+  query ProjectQuery() {
+    project(where: {slug: "${slug}"}) {
+      pageThumbnail {
+        url
+      }
+      thumbnail {
+        url
+      }
+      sections {
+        title
+        image {
+          url
+        }
+      }
+      title
+      shortDescription
+      description {
+        raw
+        text
+      }
+      technologies {
+        name
+      }
+      liveProjectUrl
+      githubUrl
+    }
+  }
+  `
+
+  return fetchHygraphQuery(query, 60 * 60 * 24)
+}
+
+export default async function Project( { params: { slug } }: ProjectProps) {
+  const { project } = await getProjectDetails(slug)
+
   return (
     <>
-      <ProjectDetails />
+      <ProjectDetails project={project} />
     </>
   )
 }
