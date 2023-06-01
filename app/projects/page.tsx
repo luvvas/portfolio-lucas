79104ad3f8478eecd40a2c277e11c8977a2082f5
luvvas <lucasmachado1@ufpr.br>
2023-06-01 16:31:18 -0300

    feat: add projects query

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,30 @@
 import { PageIntroduction } from '../components/pages/projects/page-introduction'
+import { fetchHygraphQuery } from '../utils/fetch-hygraph-query'
 import { ProjectsList } from './projects-list'
 
-export default function Projects() {
+const getPageData = async () => {
+  const query = `
+    query ProjectsQuery {
+      projects {
+        shortDescription
+        slug
+        title
+        thumbnail {
+          url
+        }
+        technologies {
+          name
+        }
+      }
+    }
+  `
+
+  return fetchHygraphQuery(query, 60 * 60 * 24)
+}
+
+export default async function Projects() {
+  const { projects } = await getPageData()
+
   return (
     <>
       <PageIntroduction />
